0d8dfc87f648e234490e27e0694dd6bee5b240b6
wynne <wynnewu@live.cn>
2023-02-23 09:05:31 +0800

    General deploy

---
+++ deploys/config/config.sh
@@ -0,0 +1,3 @@
+export DOCKER_USERNAME="zjlabbrainscience"
+export DOCKER_TOKEN="dckr_pat_2PDLJLgitGJ6zztpsVtGVgGCz3s"
+export DOCKER_STACK="platform"
---
+++ deploys/config/config_PRODUCTION.sh
@@ -0,0 +1,4 @@
+export REPLICAS="2"
+export HOST="10.11.40.170"
+export USER="zjlab"
+
