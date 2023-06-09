e19928c9fbd287798c4affd04d189fdb14344dd5
蔡涛 <caitao@zhejianglab.com>
2023-06-09 09:36:54 +0800

    添加start-miniserve.sh

---
+++ miniserve/start-miniserve.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sharePath=/zjbs-data/share
+uid="$(id -u zjshare)"
+gid="$(id -g zjshare)"
+port=2000
+
+docker stop miniserve
+docker rm miniserve
+
+docker run \
+  --volume "${sharePath}:/data" \
+  --publish "${port}:8080" \
+  --user "${uid}:${gid}" \
+  --name miniserve \
+  --detach \
+  svenstaro/miniserve:alpine \
+  --hidden \
+  --upload-files \
+  --mkdir \
+  --enable-tar-gz \
+  --dirs-first \
+  --title zjbs-data \
+  --show-symlink-info \
+  --show-wget-footer \
+  /data
