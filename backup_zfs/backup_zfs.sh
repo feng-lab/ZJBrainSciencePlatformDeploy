09039ed61443d1527dcc8242e0d84f6321cd4aff
蔡涛 <caitao@zhejianglab.com>
2023-06-20 10:28:30 +0800

    添加backup_zfs.sh

---
+++ backup_zfs/backup_zfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 配置
+dataset=zjbs-data
+snapshotCount=14
+
+# 创建新快照
+snapshotName="$(date +'%Y%m%d-%H%M%S')"
+zfs snapshot "${dataset}@${snapshotName}"
+
+# 删除旧快照
+for snapshotToDelete in $(zfs list -H -t snapshot -o name -S creation "$dataset" | head -n "-${snapshotCount}"); do
+  zfs destroy -v "$snapshotToDelete"
+done
